834800a9d8f9cba99f0305dfbacd782a2255d545
nyongtory818 <nyongtory818@users.noreply.github.com>
2018-11-28 14:50:59 -0500

    error detection

--- doc/error detection.R
+++ doc/error detection.R
@@ -1,15 +1,18 @@
+##############################################################
+##############################################################
+##############################################################
+
+### Error Detection
+### Project 4 Group 7
 
 ### load required packages
 library(ngram)
 library(NLP)
 library(tm)
 library(purrr)
-library(qdap) # 
-# library(reshape2)
+library(qdap)
 
-### please change the working directory to the folder containing this file
-setwd("/Users/hongru/Documents/GitHub/Fall2018-Project4-sec1proj4_grp6/Github/doc")
-
+### change the working directory to the folder containing this file (doc folder)
 source("../lib/bigramize.R")
 source("../lib/get_text.R")
 
@@ -24,23 +27,22 @@ ground_truth_onelines <- lapply(ground_truth_file_path,get_text)
 ground_truth_words_cleaned <- unlist(lapply(ground_truth_onelines,clean_text))
 ground_truth_by_length <- sort_by_length(ground_truth_words_cleaned)
 save(ground_truth_by_length, file = "../output/ground_truth_by_length.Rdata")
+
 ### bigramize the word from word length >= 3
-### should be modified to include word length <= 2
 num_len <- length(ground_truth_by_length)
 ground_truth_bigram_from3 <- lapply(ground_truth_by_length[3:num_len],bigramize)
-#save(ground_truth_bigram_from3,file="../output/ground_truth_bigram_from3.Rdata")
+save(ground_truth_bigram_from3,file="../output/ground_truth_bigram_from3.Rdata")
 # load("../output/ground_truth_bigram_from3.Rdata")
-### romove duplicate bigrams
-ground_truth_unibig_from3 <- lapply(ground_truth_bigram_from3,unique_bigram)
-#save(ground_truth_unibig_from3,file="../output/ground_truth_bigram_from3.Rdata") ### this is the final dictionary for error detection
-#load("../output/ground_truth_bigram_from3.Rdata")
 
 ### include word length <= 2
-ground_truth_bigram_from3$l_1$PD_1_1<-letters
+ground_truth_bigram_from3$l_1$PD_1_1 <- letters
 ground_truth_bigram_from3$l_2$PD_1_2 <- unique(ground_truth_by_length[[2]])
-ground_truth_bigram_from<-ground_truth_bigram_from3[c(29,30,1:15)]
-save(ground_truth_bigram_from ,file = "../output/dictionary.Rdata")
+ground_truth_bigram_from3 <- ground_truth_bigram_from3[c(29,30,1:18)]
 
+### romove duplicate bigrams
+final_dictionary <- lapply(ground_truth_bigram_from3,unique_bigram)
+save(final_dictionary,file="../output/dictionary.Rdata") ### this is the final dictionary for error detection
+#load("../output/dictionary.Rdata")
 
 #### tesseract #################################################################
 ### read files
@@ -48,11 +50,11 @@ tesseract_dir <- "../data/tesseract"
 tesseract_file_name <- list.files(tesseract_dir)
 tesseract_file_path <- paste0(tesseract_dir,'/',tesseract_file_name)
 tesseract_onelines <- lapply(tesseract_file_path,get_text)
+
 ### clean the texts
 tesseract_words_cleaned <- lapply(tesseract_onelines,clean_text)
 save(tesseract_words_cleaned,file="../output/tesseract_words_cleaned.Rdata")
 # load("../output/ground_truth_words_cleaned.Rdata")
-
 
 #### error detection ###########################################################
 ### these codes will be convert to a function later.
@@ -66,7 +68,7 @@ for (i in 1:length(tesseract_words_cleaned)) {
     cur_word <- cur_tesseract[j]
     cur_length <- nchar(cur_word)
     cur_bigram <- bigramize_word(cur_word)
-    cur_dic <- ground_truth_unibig_from3[[paste("l_",cur_length,sep="")]]
+    cur_dic <- final_dictionary[[paste("l_",cur_length,sep="")]]
     tmp_lable <- c()
     # browser()
     for (k in 1:length(cur_bigram)){
@@ -80,17 +82,13 @@ for (i in 1:length(tesseract_words_cleaned)) {
 
 error_rate <- 1-(sapply(tesseract_labels, sum)/sapply(tesseract_words_cleaned, length))
 summary(error_rate)
-   
+
 save(tesseract_labels,file="../output/tesseract_labels.Rdata")
 
-
-detected_words<-list()
-for(i in 1:100){
-  detected_words[[i]]<-tesseract_words_cleaned[[i]][!as.logical(tesseract_labels[[i]])]
+### save the error words
+detected_words <- list()
+for (i in 1:100){
+  detected_words[[i]] <- tesseract_words_cleaned[[i]][!as.logical(tesseract_labels[[i]])]
 }
 save(detected_words,file="../output/detected_words.Rdata")
-
-
-
-
 
